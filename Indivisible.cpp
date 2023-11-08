4a06724b03b5867b21167583d2330387856e6362
emon4075 <mdabdullahemon4075@gmail.com>
2023-11-08 20:23:15 +0600

    8/11/2023

---
+++ Indivisible.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C;
+        cin >> A >> B >> C;
+        for (int i = 1; i < 100; i++)
+        {
+            if (((A % i) != 0) && ((B % i) != 0) && ((C % i) != 0))
+            {
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
